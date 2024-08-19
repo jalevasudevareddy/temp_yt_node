d3db2bc69ea2cd1d08be118a54723269092f68eb
VasudevaReddy Jale <103563671+jalevasudevareddy@users.noreply.github.com>
2024-08-19 12:59:38 +0530

    commint after crating node application on 19-08-2024

--- app.js
+++ app.js
@@ -28,7 +28,50 @@
 // console.log(path.sep);
 // console.log(path.join(__dirname));
 
-const lodash = require("lodash");
-const item = [1, [2, [3, [4]]]];
-const newItem = lodash.flattenDeep(item);
-console.log(newItem);
+// const lodash = require("lodash");
+// const item = [1, [2, [2, [4]]]];
+// const newItem = lodash.flattenDeep(item);
+// console.log(newItem);
+//----------------------------------------------------------------
+// const EventEmitter = require("events");
+
+// const customEventEmitter = new EventEmitter();
+// customEventEmitter.on("response", () => {
+//   console.log("Data received: ");
+// });
+
+// customEventEmitter.on("response", (name, id) => {
+//   console.log(`Data received with user name ${name} and the id ${id} `);
+// });
+
+// customEventEmitter.emit("response", "vasu", 32);
+//----------------------Streams-----------------------------------------------
+
+// const { createReadStream } = require("fs");
+// const server = require("http");
+// const stream = createReadStream("./content/big.txt", {
+//   highWaterMark: 20000,
+//   encoding: "utf8",
+// });
+
+// stream.on("data", (data) => {
+//   console.log("Received data...", data);
+// });
+
+// stream.on("error", (err) => {
+//   console.log("Error while reading the file", err);
+// });
+
+// var fs = require("fs");
+// server
+//   .createServer(function (req, res) {
+//     const fileStream = createReadStream("./content/big.txt", "utf8");
+//     fileStream.on("open", () => {
+//       fileStream.pipe(res);
+//     });
+//     const text = fs.readFileSync("./content/big.txt", "utf8");
+//     // res.end(text);
+//   })
+//   .listen(5000);
+
+//--------------------------------------------------------------
